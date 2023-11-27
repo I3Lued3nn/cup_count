f12538a843c99406c9039755e2aaf2c6353bfbb8
l3Luedenn <97693688+I3Lued3nn@users.noreply.github.com>
2023-11-27 14:06:43 -0300

    Add files via upload

--- contador de copos de agua/app.js
+++ contador de copos de agua/app.js
@@ -1,6 +1,7 @@
 const menos = document.getElementById("menos");
 const mais = document.getElementById("mais");
 const cups = document.getElementById("cups");
+const cup = document.getElementById("cup");
 const volu = document.getElementById("volu");
 
 let result = 0;
@@ -9,6 +10,7 @@ let litros = 0;
 mais.onclick = function(){
     result++;
     cups.textContent = result;
+    cup.textContent = result;
 
     litros += 200;
     volu.textContent = litros + "ml";
@@ -22,14 +24,16 @@ mais.onclick = function(){
 menos.onclick = function(){
      if(result <= 0){
         return;
-    } 
-    result--;
-    cups.textContent = result;
-    
-   if(litros <= 0){
-        litros = 200 ;
     }
     
+    result--;
+    cups.textContent = result;
+    cup.textContent = result;
+   
+    if(litros <= 0){
+        return;
+    }
+
     litros -= 200;
     volu.textContent = litros + "ml";
     if(litros >= 1000){
