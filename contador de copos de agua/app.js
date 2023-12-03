f8c33d5bb0723d3f51a1a8a5c627f456df76cfd6
l3Luedenn <97693688+I3Lued3nn@users.noreply.github.com>
2023-12-03 11:06:47 -0300

    Add files via upload

--- contador de copos de agua/app.js
+++ contador de copos de agua/app.js
@@ -3,29 +3,22 @@ const mais = document.getElementById("mais");
 const cups = document.getElementById("cups");
 const cup = document.getElementById("cup");
 const volu = document.getElementById("volu");
-
 let result = 0;
 let litros = 0;
-
 mais.onclick = function(){
     result++;
     cups.textContent = result;
     cup.textContent = result;
-
     litros += 200;
     volu.textContent = litros + "ml";
     if(litros >= 1000){
         volu.textContent = litros/1000 + "L"
     }
-
 }
-
-
 menos.onclick = function(){
      if(result <= 0){
         return;
     }
-    
     result--;
     cups.textContent = result;
     cup.textContent = result;
@@ -33,12 +26,34 @@ menos.onclick = function(){
     if(litros <= 0){
         return;
     }
-
     litros -= 200;
     volu.textContent = litros + "ml";
     if(litros >= 1000){
         volu.textContent = litros/1000 + "L"
     }
-
-    
 }
+function calcular(){
+    const valor = document.getElementById("peso").value;
+    const numero = parseFloat(valor);
+    const exe = (numero * 35) / 1000;
+    document.getElementById("resultado").innerHTML = exe + " Litros";
+}
+
+document.addEventListener("keypress", function(e) {
+    if(e.key === 'Enter') {
+        const peso = document.getElementById("peso").value;
+        let pesoEmGramas = (peso * 35) / 1000;
+        document.getElementById("resultado").innerHTML = pesoEmGramas + " Litros";
+        peso.click();
+    }
+});
+
+/*
+
+function calcular() {
+
+    let peso = document.getElementById("peso").value;
+    let pesoEmGramas = (peso * 35) / 1000;
+    document.getElementById("resultado").innerHTML = pesoEmGramas + " Litros";
+} 
+ */
